fbf8250045a0c49538018d54247182092480bb8a
Joshua Ferris <jajjferris@gmail.com>
2019-11-12 21:57:01 -0500

    updated new user email

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,45 @@
+/* eslint-disable no-await-in-loop */
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const sgMail = require('@sendgrid/mail');
 
 admin.initializeApp();
+const db = admin.firestore();
 
 const SENDGRID_API_KEY = functions.config().sendgrid.key;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-exports.firestoreEmail = functions.firestore.document('users/{userId}').onCreate(async (event, context) => {
-  const userId = context.params.userId;
-  const db = admin.firestore();
-  const emails = [];
-  const userDoc = await db.collection('users').doc(userId).get();
-  const user = userDoc.data();
-  const allAdmins = await db.collection('admins').get();
-  await Promise.all(allAdmins.docs.map(async (curAdmin) => {
-    const isAdminData = curAdmin.data();
-    if (!isAdminData.admin) return;
-    const adminDataDoc = await db.collection('users').doc(curAdmin.id).get();
-    const { email, displayName } = adminDataDoc.data();
-    emails.push({
-      to: email,
-      from: 'no-reply@yscfoh.com',
-      subject: 'Clickr - New User',
-      templateId: 'd-6ead4d786f4e4102826c1f14dc2fe414',
-      dynamic_template_data: {
-        name: displayName,
-        newUser: user.email,
-      },
-    });
-  }));
+const getAdmins = async () => {
+  const adminsQuerySnapshot = await db.collection('admins').get();
+  const adminsStatus = adminsQuerySnapshot.docs.reduce((acc, cur) => ({
+    ...acc,
+    [cur.id]: cur.data().admin,
+  }), {});
+  const admins = [];
+  for (const [key, value] of Object.entries(adminsStatus)) {
+    if (value) {
+      const adminDataDoc = await db.collection('users').doc(key).get();
+      const { email, displayName } = adminDataDoc.data();
+      admins.push({
+        email,
+        displayName,
+      });
+    }
+  }
+  return admins;
+};
+
+exports.newUserEmails = functions.auth.user().onCreate(async (user) => {
+  const admins = await getAdmins();
+  const emails = await Promise.all(admins.map(el => ({
+    to: el.email,
+    from: 'no-reply@yscfoh.com',
+    subject: 'Clickr - New User',
+    templateId: 'd-6ead4d786f4e4102826c1f14dc2fe414',
+    dynamic_template_data: {
+      name: el.displayName,
+      newUser: user.email,
+    },
+  })));
   await sgMail.send(emails);
 });
